a9631e2579b8ef4dcd91bc956108f4699deedec4
Matt <mspillit@gmail.com>
2021-04-20 18:07:53 -0500

    adding search

--- Client/scripts/adminreports.js
+++ Client/scripts/adminreports.js
@@ -1,3 +1,8 @@
+function onLoad() {
+    getCheckedOut();
+    getUserIDs();
+}
+
 function getInvItems() {
     const url = "https://localhost:5001/API/Item";
     
@@ -48,25 +53,41 @@ function goBack() {
     history.back();
 }
 
-function getUserIDs(i) {
+function getUserIDs() {
     const url = "https://localhost:5001/API/User";
 
     fetch(url).then(function(response){
         console.log(response);
         return response.json();
     }).then(function(json){
-        let html = "<search>";
+        let html = "";
         json.forEach((user)=>{
             html += "<option value=\"" + user.userid + "\">" + user.userid;
             html += "; " + user.userfname + " " + user.userlname;
             html += "</option>";
         });
-        html += "</search>";
         document.getElementById("emplsearch").innerHTML = html;
     });
 }
 
+//doesn't work atm
+
 function filterCheckedOut() {
-    var userid = document.getElementById().value;
-    const url = "https://localhost:5001/API/User" + userid;
+    var filter = document.getElementById("emplsearch").value;
+    const url = "https://localhost:5001/API/CheckedOutItem";
+
+    fetch(url).then(function(response){
+        console.log(response);
+        return response.json();
+    }).then(function(json){
+        let html = "<ul id=\"list\">";
+        json.forEach((item)=>{
+            html += "<li class=\"flex\"><div class=\"picture\"></div>"; 
+            html += "&ensp;" + item.itemid + "&emsp;" + item.itemname + "&emsp;" + item.userfname + " " + item.userlname + "&emsp;" + (new Date(Date.parse(item.duedate))).toLocaleDateString('en-US');
+        });
+        html += "</ul>";
+        document.getElementById("coitems").innerHTML = html;
+    }).catch(function(error){
+        console.log(error);
+    });
 }
